921407f3476d6b66a474e86c0decaabf86b96df8
HiLize <hi_lize@163.com>
2018-10-19 18:12:09 +0800

    menu.js

--- src/store/modules/menu.js
+++ src/store/modules/menu.js
@@ -3,68 +3,68 @@ import util from 'util'
 const app = {
     state: {
         menuList: [
-            {
-                name: 'tribe',
-                title: '群聊管理',
-                icon: 'ios-gear',
-                children: [
-                    {
-                        name: 'tribe',
-                        title: '群聊管理',
-                        icon: 'ios-gear',
-                        path: 'http://172.20.6.218:8000/admin.html#/'
-                    }
-                ]
-            },
-            {
-                name: 'crossTalk',
-                title: '话题管理',
-                icon: 'ios-gear',
-                children: [
-                    {
-                        name: 'crosstalk',
-                        title: '话题管理',
-                        icon: 'ios-paper',
-                        path: 'http://172.20.6.218:8000/admin.html#/crosstalkmanage'
-                    },
-                    {
-                        name: 'crosstalkcheck',
-                        title: '话题稿征集审核',
-                        icon: 'ios-paper',
-                        path: 'http://172.20.6.218:8000/admin.html#/crosstalkmanage/checkmanage'
-                    }
-                ]
-            },
-            {
-                name: 'circlecontent',
-                title: '动态审核管理',
-                icon: 'ios-gear',
-                children: [
-                    {
-                        name: 'circlecontent',
-                        title: '动态审核管理',
-                        icon: 'ios-gear',
-                        path: 'http://172.20.6.218:8000/admin.html#/circlecontentmanage'
-                    }
-                ]
-            },
-            {
-                name: 'yibanlog',
-                title: '易班操作日志',
-                icon: 'ios-gear',
-                children: [
-                    {
-                        name: 'yibanlog',
-                        title: '易班操作日志',
-                        icon: 'ios-gear',
-                        path: 'http://172.20.6.218:8000/admin.html#/yibanlog'
-                    }
-                ]
-            }
+            // {
+            //     name: 'tribe',
+            //     title: '群聊管理',
+            //     icon: 'ios-gear',
+            //     children: [
+            //         {
+            //             name: 'tribe',
+            //             title: '群聊管理',
+            //             icon: 'ios-gear',
+            //             path: 'http://172.20.6.218:8000/admin.html#/'
+            //         }
+            //     ]
+            // },
+            // {
+            //     name: 'crossTalk',
+            //     title: '话题管理',
+            //     icon: 'ios-gear',
+            //     children: [
+            //         {
+            //             name: 'crosstalk',
+            //             title: '话题管理',
+            //             icon: 'ios-paper',
+            //             path: 'http://172.20.6.218:8000/admin.html#/crosstalkmanage'
+            //         },
+            //         {
+            //             name: 'crosstalkcheck',
+            //             title: '话题稿征集审核',
+            //             icon: 'ios-paper',
+            //             path: 'http://172.20.6.218:8000/admin.html#/crosstalkmanage/checkmanage'
+            //         }
+            //     ]
+            // },
+            // {
+            //     name: 'circlecontent',
+            //     title: '动态审核管理',
+            //     icon: 'ios-gear',
+            //     children: [
+            //         {
+            //             name: 'circlecontent',
+            //             title: '动态审核管理',
+            //             icon: 'ios-gear',
+            //             path: 'http://172.20.6.218:8000/admin.html#/circlecontentmanage'
+            //         }
+            //     ]
+            // },
+            // {
+            //     name: 'yibanlog',
+            //     title: '易班操作日志',
+            //     icon: 'ios-gear',
+            //     children: [
+            //         {
+            //             name: 'yibanlog',
+            //             title: '易班操作日志',
+            //             icon: 'ios-gear',
+            //             path: 'http://172.20.6.218:8000/admin.html#/yibanlog'
+            //         }
+            //     ]
+            // }
         ]
     },
     mutations: {
-        increment (state, newData) {
+        updateState (state, newData) {
             state.menuList = newData
         }
     },
@@ -73,7 +73,7 @@ const app = {
             util.httpGet(api.userOwner,{},{}).then(res => {
                 if(res && res.code == '0'){
                     let menus = vm.rebuildMenuList(res.datas.adminResList)
-                    context.commit('increment', menus)
+                    context.commit('updateState', menus)
                 }
             })
         }
