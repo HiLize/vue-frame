eb47a66d70b8202b41dcabd73a868c13b227a8ab
HiLize <hi_lize@163.com>
2018-11-01 16:29:47 +0800

    修改icon名称

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -23,7 +23,7 @@
             <div class="main-header">
                 <div class="navicon-con">
                     <Button :style="{transform: 'rotateZ(' + (this.shrink ? '-90' : '0') + 'deg)'}" type="text" @click="toggleClick">
-                        <Icon type="navicon" size="32"></Icon>
+                        <Icon type="ios-menu" size="32" />
                     </Button>
                 </div>
                 <div class="header-middle-con">
